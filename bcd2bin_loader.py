0f37a59f18558b0aa0e3e291479d6c021e5f3ded
ljb2405 <jbyoon@princeton.edu>
2024-02-27 05:42:47 +0000

    init edits

--- bcd2bin_loader.py
+++ bcd2bin_loader.py
@@ -1,6 +1,13 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
+# Parameters
+reset = 0
+prog_wait = 1
+programming = 2
+prog_stabilizing = 3
+prog_done = 4
+
 # Pin configuration
 
 # PROG_CLK = # clock probably should be dealt with PWM
@@ -12,18 +19,25 @@ PROG_DOUT = "P8_11"
 PROG_WE_O = "P8_12"
 
 # Setup GPIO
-GPIO.setup(PROG_B, GPIO.OUT)
-GPIO.setup(CCLK, GPIO.OUT)
-GPIO.setup(DIN, GPIO.OUT)
-GPIO.setup(DONE, GPIO.IN)
+GPIO.setup(PROG_RST, GPIO.OUT)
+GPIO.setup(PROG_DONE, GPIO.OUT)
+GPIO.setup(PROG_WE, GPIO.OUT)
+GPIO.setup(PROG_DIN, GPIO.OUT)
+GPIO.setup(PROG_DOUT, GPIO.IN)
+GPIO.setup(PROG_WE_O, GPIO.IN)
 
 # Initialize
-GPIO.output(PROG_B, GPIO.HIGH)
-GPIO.output(CCLK, GPIO.LOW)
-time.sleep(0.1)  # Wait for any power-up conditions to stabilize
-GPIO.output(PROG_B, GPIO.LOW)
-time.sleep(0.01)  # Wait for the FPGA to clear any previous configuration
-GPIO.output(PROG_B, GPIO.HIGH)
+GPIO.output(PROG_RST, GPIO.HIGH)
+GPIO.output(PROG_DONE, GPIO.LOW)
+GPIO.output(PROG_WE, GPIO.LOW)
+GPIO.output(PROG_WE, GPIO.LOW)
+
+# Initialize variables
+state = reset
+prog_progress = 0
+wait_count = 0
+
+
 
 # Function to send a single bit to the FPGA
 def send_bit(bit):
@@ -33,14 +47,23 @@ def send_bit(bit):
     GPIO.output(CCLK, GPIO.LOW)
 
 # Load the bitstream file
-with open("/home/jae/order/bcd2bin/bitgen.out", "rb") as bitstream:
-    byte = bitstream.read(1)
-    while byte:
-        # Send each bit in the byte (assuming MSB first)
-        for i in range(7, -1, -1):
-            bit = (byte[0] >> i) & 1
-            send_bit(bit)
-        byte = bitstream.read(1)
+bitstream = open("/home/jae/order/bcd2bin/bitgen.out", "rb")
+    # byte = bitstream.read(1)
+    # while byte:
+    #     # Send each bit in the byte (assuming MSB first)
+    #     for i in range(7, -1, -1):
+    #         bit = (byte[0] >> i) & 1
+    #         send_bit(bit)
+    #     byte = bitstream.read(1)
+
+# Programmer FSM
+while (state != prog_done):
+    if (state == reset):
+        state = prog_wait
+        # time.sleep(1)
+    elif (state == prog_wait):
+        
+
 
 # Check if programming was successful
 if GPIO.input(DONE):
