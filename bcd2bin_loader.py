fdd16761e551631c29a25a7420a25b9559c2ec14
ljb2405 <jbyoon@princeton.edu>
2024-02-27 23:12:07 +0000

    init loader

--- bcd2bin_loader.py
+++ bcd2bin_loader.py
@@ -7,10 +7,11 @@ prog_wait = 1
 programming = 2
 prog_stabilizing = 3
 prog_done = 4
+half_period = 12.5e-9
 
 # Pin configuration
 
-# PROG_CLK = # clock probably should be dealt with PWM
+PROG_CLK = "P8_14" # clock probably should be dealt with PWM
 PROG_RST = "P8_7"
 PROG_DONE = "P8_8"
 PROG_WE = "P8_9"
@@ -19,6 +20,7 @@ PROG_DOUT = "P8_11"
 PROG_WE_O = "P8_12"
 
 # Setup GPIO
+GPIO.setup(PROG_CLK, GPIO.OUT)
 GPIO.setup(PROG_RST, GPIO.OUT)
 GPIO.setup(PROG_DONE, GPIO.OUT)
 GPIO.setup(PROG_WE, GPIO.OUT)
@@ -31,6 +33,7 @@ GPIO.output(PROG_RST, GPIO.HIGH)
 GPIO.output(PROG_DONE, GPIO.LOW)
 GPIO.output(PROG_WE, GPIO.LOW)
 GPIO.output(PROG_WE, GPIO.LOW)
+GPIO.output(PROG_CLK, GPIO.LOW)
 
 # Initialize variables
 state = reset
@@ -41,10 +44,12 @@ wait_count = 0
 
 # Function to send a single bit to the FPGA
 def send_bit(bit):
-    GPIO.output(DIN, bit)
-    GPIO.output(CCLK, GPIO.HIGH)
-    time.sleep(0.0001)  # Adjust based on your FPGA's clock requirements
-    GPIO.output(CCLK, GPIO.LOW)
+    GPIO.output(PROG_WE, GPIO.HIGH)
+    GPIO.output(PROG_DIN, bit)
+    GPIO.output(PROG_CLK, GPIO.HIGH)
+    time.sleep(half_period)  # Adjust based on your FPGA's clock requirements
+    GPIO.output(PROG_CLK, GPIO.LOW)
+    GPIO.output(PROG_WE, GPIO.LOW)
 
 # Load the bitstream file
 bitstream = open("/home/jae/order/bcd2bin/bitgen.out", "rb")
@@ -56,17 +61,22 @@ bitstream = open("/home/jae/order/bcd2bin/bitgen.out", "rb")
     #         send_bit(bit)
     #     byte = bitstream.read(1)
 
-# Programmer FSM
-while (state != prog_done):
-    if (state == reset):
-        state = prog_wait
-        # time.sleep(1)
-    elif (state == prog_wait):
-        
+#while (GPIO.input(PROG_DONE) == GPIO.LOW):
+byte = bitstream.read(1)
+while byte:
+    # Send each bit in the byte (assuming MSB first)
+    for i in range(7, -1, -1):
+        bit = (byte[0] >> i) & 1
+        send_bit(bit)
+    byte = bitstream.read(1)
 
+GPIO.output(PROG_DONE, GPIO.HIGH)
+    
 
 # Check if programming was successful
-if GPIO.input(DONE):
+
+
+if GPIO.input(PROG_DONE):
     print("FPGA programming successful!")
 else:
     print("FPGA programming failed.")
