2e93f468709e906aa0a6b922092541d9f0adef57
ljb2405 <jbyoon@princeton.edu>
2024-03-30 13:15:45 -0400

    toy test

--- loader_test.py
+++ loader_test.py
@@ -2,9 +2,9 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 # Parameters
-half_period = 5e-2 # 10 kHz --> In reality, something lower than 10 KHz because of how Python runs
+half_period = 5e-4 # 10 kHz --> In reality, something lower than 10 KHz because of how Python runs
 sleepTime100Cycle = half_period * 2 * 100
-BS_NUM_QWORDS       = 422 # Bitstream length
+BS_NUM_QWORDS       = 1 # Bitstream length
 BS_WORD_SIZE        = 1 # Byte
 # Pin configuration
 # general clock supplied by PWM
@@ -40,33 +40,22 @@ GPIO.setup(gpio, GPIO.IN)
 GPIO.output(PROG_RST, GPIO.HIGH)
 GPIO.output(PROG_DONE, GPIO.LOW)
 GPIO.output(PROG_WE, GPIO.LOW)
+GPIO.output(PROG_WE, GPIO.LOW)
 GPIO.output(PROG_CLK, GPIO.LOW)
 
 # Computes prog_fragment for programming stabilization purposes
 def compute_fragments():
-    global prog_we
-    global prog_we_prev
-    global prog_we_o
-    global prog_we_o_prev
-    global prog_fragments
-
     prog_we_prev = prog_we
     prog_we_o_prev = prog_we_o
     prog_we_o = GPIO.input(PROG_WE_O)
 
-    if ((prog_we_prev and ~prog_we) and ~(prog_we_o_prev and ~prog_we_o)):
+    if (prog_we_prev and ~prog_we) and ~(prog_we_o_prev and ~prog_we_o):
         prog_fragments += 1
     elif (~(prog_we_prev and ~prog_we) and (prog_we_o_prev and ~prog_we_o)):
         prog_fragments -= 1
     
 # Function to send a single bit to the FPGA
 def send_bit(bit):
-    global prog_we
-    global prog_we_prev
-    global prog_we_o
-    global prog_we_o_prev
-    global prog_fragments
-
     GPIO.output(PROG_DIN, bit)
     prog_we_prev = prog_we
 
@@ -77,17 +66,12 @@ def send_bit(bit):
     
     time.sleep(half_period)  # Adjust based on your FPGA's clock requirements
 
-    if ((prog_we_prev & ~prog_we) & ~(prog_we_o_prev & ~prog_we_o)):
+    if (prog_we_prev and ~prog_we) and ~(prog_we_o_prev and ~prog_we_o):
         prog_fragments += 1
-    elif (~(prog_we_prev & ~prog_we) & (prog_we_o_prev & ~prog_we_o)):
+    elif (~(prog_we_prev and ~prog_we) and (prog_we_o_prev and ~prog_we_o)):
         prog_fragments -= 1
     
-    GPIO.output(PROG_WE, GPIO.LOW)
     GPIO.output(PROG_CLK, GPIO.LOW)
-    time.sleep(half_period/2)
-    GPIO.output(PROG_WE, GPIO.LOW)
-    GPIO.output(PROG_WE, GPIO.LOW)
-    prog_we = 0
     time.sleep(half_period)
 
 def prog_sleep(cycles):
@@ -100,7 +84,7 @@ def prog_sleep(cycles):
         time.sleep(half_period)
 
 # Load the bitstream file
-bitstream = open("/home/jae/order/bcd2bin/bitgen.out", "rb")
+bitstream = open("/home/jae/order/bcd2bin/test_bitgen.out", "rb")
 
 GPIO.output(PROG_RST, GPIO.LOW)
 
@@ -114,8 +98,7 @@ if (GPIO.input(gpio) == GPIO.HIGH):
     print("gpio pin set high")
     byte = bitstream.read(1)
     prog_progress = 0
-    prog_we = 0
-    prog_we_o = GPIO.input(PROG_WE_O)
+
     while (prog_progress + BS_WORD_SIZE < BS_NUM_QWORDS * 8):
         GPIO.output(PROG_WE, GPIO.HIGH)
     # Send each bit in the byte (assuming MSB first)
@@ -129,11 +112,9 @@ if (GPIO.input(gpio) == GPIO.HIGH):
         prog_sleep(1)
     # Stabilizing Phase
 
-    while (prog_fragments > 0):
+    while True:
         prog_sleep(1)
-        compute_fragments()
-        print("prog_fragments: %i\n", prog_fragments)
-    prog_sleep(100)
+
     GPIO.output(PROG_DONE, GPIO.HIGH)
     time.sleep(sleepTime100Cycle)
 
